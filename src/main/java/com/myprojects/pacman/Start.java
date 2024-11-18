5c65df551a6d8ecd0f747127245f7f6715a8d743
Davide Mancini <davide.mancini10@studio.unibo.it>
2024-11-18 16:49:16 +0100

    Made the game run on program start

--- src/main/java/com/myprojects/pacman/Start.java
+++ src/main/java/com/myprojects/pacman/Start.java
@@ -1,10 +1,12 @@
 package com.myprojects.pacman;
 
+import com.myprojects.pacman.api.Game;
 import com.myprojects.pacman.impl.Pacman;
 
 public class Start {
 
 	public static void main(final String[] args) {
-		new Pacman();
+		final Game pacman = new Pacman();
+		pacman.run();
 	}
 }
